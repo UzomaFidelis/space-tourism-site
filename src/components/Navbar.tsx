c340110249ce96320207977181323e7b7e5e697a
Uzoma Fidelis <fiddycena@gmail.com>
2024-04-28 16:59:31 +0100

    added navmenu sidebar(mobile) navlink active style

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   const activeNavStyle =
-    "md:after:content-[''] md:after:absolute md:after:bottom-0 md:after:left-[48%] md:after:translate-x-[-50%] md:after:w-[100%] md:after:h-[0.1rem] md:after:bg-white lg:after:h-[0.2rem]";
+    "after:content-[''] after:absolute after:right-0 after:bottom-0 after:h-[50%] after:translate-y-[-50%] after:w-[0.2rem] after:bg-white md:after:translate-y-0 md:after:bottom-0 md:after:left-[48%] md:after:translate-x-[-50%] md:after:w-[100%] md:after:h-[0.1rem] md:after:bg-white lg:after:h-[0.2rem]";
 
   const inactiveNavStyle =
     "md:hover:after:content-[''] md:hover:after:absolute md:hover:after:bottom-0 md:hover:after:left-[48%] md:hover:after:translate-x-[-50%] md:hover:after:w-[100%] md:hover:after:h-[0.1rem] md:hover:after:bg-purply lg:hover:after:h-[0.2rem]";
@@ -10,7 +10,7 @@ const Navbar = () => {
   return (
     <nav className="mt-[3.2rem] md:mt-0 md:pr-[0.5rem]">
       <ul className="uppercase font-barlow-con text-left pl-[1.1rem] tracking-[0.2em] md:flex md:gap-[2.3rem] md:tracking-[0.13em] lg:gap-[4.3rem]">
-        <li className="py-[0.8rem] md:text-[0.9rem] md:relative lg:py-[2rem]">
+        <li className="py-[0.8rem] relative md:text-[0.9rem] lg:py-[2rem]">
           <NavLink
             to="/"
             className={({ isActive }) =>
@@ -20,7 +20,7 @@ const Navbar = () => {
             <span className="font-bold md:hidden lg:inline">00</span> home
           </NavLink>
         </li>
-        <li className="py-[0.8rem] md:text-[0.9rem] md:relative lg:py-[2rem]">
+        <li className="py-[0.8rem] relative md:text-[0.9rem]  lg:py-[2rem]">
           <NavLink
             to="/destination"
             className={({ isActive }) =>
@@ -31,7 +31,7 @@ const Navbar = () => {
             destination
           </NavLink>
         </li>
-        <li className="py-[0.8rem] md:text-[0.9rem] md:relative lg:py-[2rem]">
+        <li className="py-[0.8rem] relative md:text-[0.9rem]  lg:py-[2rem]">
           <NavLink
             to="/crew"
             className={({ isActive }) =>
@@ -41,7 +41,7 @@ const Navbar = () => {
             <span className="font-bold md:hidden lg:inline">02</span> crew
           </NavLink>
         </li>
-        <li className="py-[0.8rem] md:text-[0.9rem] md:relative lg:py-[2rem]">
+        <li className="py-[0.8rem] relative md:text-[0.9rem]  lg:py-[2rem]">
           <NavLink
             to="/technology"
             className={({ isActive }) =>
